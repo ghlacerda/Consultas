22ac44e97f68bd6e24aff3968ccbbed7ae4ee8bb
ghlacerda <55388763+ghlacerda@users.noreply.github.com>
2019-10-18 16:59:50 -0300

    Alter Consultas 18-10-2019

--- Incidente #109167 - Criar nova visão do painel diretoria movimentação diária.sql
+++ Incidente #109167 - Criar nova visão do painel diretoria movimentação diária.sql
@@ -111,7 +111,7 @@ Select TB1.HANDLE,
 			   Cast(IIF(GLGL_DOCUMENTOS.TIPODOCUMENTO In (22, 13), (Select Count(*) [Qtde]
 																	  From GLGL_DOCLOGASSOCIADOS
 																	 Where GLGL_DOCLOGASSOCIADOS.DOCUMENTOLOGISTICAPAI = GLGL_DOCUMENTOS.HANDLE), 1) As float)			[Qntde Docs]
-		  From GLGL_DOCUMENTOS
+		  From GLGL_DOCUMENTOS 
 		 Inner Join GLGL_FILIAIS	On GLGL_FILIAIS.HANDLE		= GLGL_DOCUMENTOS.FILIAL
 		 Inner Join FILIAIS			On FILIAIS.HANDLE			= GLGL_FILIAIS.FILIAL
 		 Inner Join ESTADOS			On ESTADOS.HANDLE			= FILIAIS.ESTADO
@@ -273,7 +273,7 @@ Select HANDLE,
 															Inner Join FILIAIS			On FILIAIS.HANDLE					= GLGL_FILIAIS.FILIAL
 															Where GLGL_DOCUMENTOS.STATUS									Not In (224, 404)
 															And GLGL_DOCUMENTOS.STATUS									Not In (220, 223, 236, 237, 417)
-															And GLGL_DOCUMENTOS.STATUSFATURA								 IN (388)
+															And GLGL_DOCUMENTOS.STATUSFATURA							not in (389,390,1034)
 															And GLGL_DOCUMENTOS.FRETECORTESIA							= 'N'
 															And (GLGL_DOCUMENTOS.TIPODOCUMENTO							In (6)
 															And GLGL_DOCUMENTOS.TIPORPS									<> 323)
@@ -290,15 +290,24 @@ Select HANDLE,
 														   And FILIAIS.HANDLE											= TB2.HANDLE ), 0), 0),2)	[Disponiveis Para faturar],
 
 		ROUND(IIF(TB2.CATEGORIA	= 'Frete Total',   IsNull( (Select Sum(GLGL_DOCUMENTOS.VALORCONTABIL)						[TOTAL]
-														 From GLGL_DOCUMENTOS
+														  From GLGL_DOCUMENTOS
 															Inner Join GLGL_FILIAIS	On GLGL_FILIAIS.HANDLE				= GLGL_DOCUMENTOS.FILIAL
 															Inner Join FILIAIS			On FILIAIS.HANDLE					= GLGL_FILIAIS.FILIAL
 															Where GLGL_DOCUMENTOS.STATUS									Not In (224, 404)
 															And GLGL_DOCUMENTOS.STATUS									Not In (220, 223, 236, 237, 417)
-															And GLGL_DOCUMENTOS.STATUSFATURA								 IN (389,390,391,1034)
+															And GLGL_DOCUMENTOS.STATUSFATURA							IN (389,390,1034)
 															And GLGL_DOCUMENTOS.FRETECORTESIA							= 'N'
 															And (GLGL_DOCUMENTOS.TIPODOCUMENTO							In (6)
 															And GLGL_DOCUMENTOS.TIPORPS									<> 323)
+															And Not Exists (Select 1
+																			 From GLGL_DOCLOGASSOCIADOS DLA
+																			Inner Join GLGL_DOCUMENTOS NFS 
+																			   On (DLA.DOCUMENTOLOGISTICAPAI			= NFS.HANDLE)
+														 					Where DLA.DOCUMENTOLOGISTICAFILHO			= GLGL_DOCUMENTOS.HANDLE
+																			  And NFS.TIPODOCUMENTO						In (1, 2, 17, 22)
+																			  And NFS.FRETECORTESIA						= 'N'
+																			  And NFS.STATUS							Not In (224, 404)
+																			  And NFS.STATUS							Not In (220, 223, 236, 237, 417))
 														   And Cast(GLGL_DOCUMENTOS.DATAEMISSAO As Date)				= TB2.Data
 														   And FILIAIS.HANDLE											= TB2.HANDLE ), 0), 0),2)	[Não dispoivel para faturar]
   From (Select HANDLE,
---
+++ Mudança #109171 -  ROUTE PLANNING BRIAN QDF.sql
@@ -0,0 +1,99 @@
+SELECT 
+	TIPOCLIENTE,
+	[TRANSIT TIME],
+	SUM(QUANTIDADECOLETA) AS QTDPEDIDO,
+	SUM(VOLUME) AS QTDVOLUME
+
+FROM(
+
+SELECT 
+
+	DL.NUMERO,
+	DL.FILIAL,                                                                                                                                                 
+	DL.VALORTOTALRECEBER AS VALORFRETE,
+	DOCCLIPESOTOTAL AS PESO,
+	DOCCLIPESOCUBADOTOTAL AS PESOCUBADO, 
+	DOCCLIPESOCONSIDERADO AS PESOCONSIDERADO,
+	COUNT(DISTINCT CP.HANDLE) AS QUANTIDADECOLETA,
+	DOCCLIPESOCONSIDERADO AS PESOCOLETAREALIZADO,
+	DOCCLIVOLUME AS [VOLUME],
+	DOCCLIVALORTOTAL AS [VALORMERCADORIA],
+	CASE
+		WHEN 
+			(CASE
+				WHEN PC.CLIENTEEPP = 'N' THEN 'FRACIONADO'
+				--WHEN DST2.NOME = 'NATURA COSMETICOS S/A' THEN 'NATURA'
+			ELSE 'EPP'
+			END) = 'FRACIONADO' AND DST2.NOME = 'NATURA COSMETICOS S/A' THEN 'NATURA'
+		ELSE (CASE
+				WHEN PC.CLIENTEEPP = 'N' THEN 'FRACIONADO'
+				--WHEN DST2.NOME = 'NATURA COSMETICOS S/A' THEN 'NATURA'
+			ELSE 'EPP'
+			END)
+	END AS TIPOCLIENTE,
+	DST2.NOME,
+	PES.DIA AS [TRANSIT TIME]
+
+FROM GLGL_DOCUMENTOASSOCIADOS DA   
+                                                                                                                                                                   
+INNER JOIN GLGL_DOCUMENTOCLIENTES DC 
+	ON DC.HANDLE = DA.DOCUMENTOCLIENTE
+	                                                                                       
+INNER JOIN GLGL_DOCUMENTOS DL 
+	ON DA.DOCUMENTOLOGISTICA = DL.HANDLE 
+	                                                                                                     
+INNER JOIN GLOP_COLETAPEDIDOS CP 
+	ON CP.HANDLE = DC.PEDIDOCOLETA
+
+LEFT JOIN GN_PESSOAS DST2 
+	ON (DST2.HANDLE = DL.DESTINATARIO) 
+
+INNER JOIN GLGL_PESSOACONFIGURACOES PC 
+	ON DST2.CODIGO = PC.HANDLE
+
+INNER JOIN [DBRodo].[dbo].[GLOP_PRAZOENTREGAS] PE WITH(NOLOCK)
+ ON DL.PRAZOENTREGA = PE.HANDLE
+
+INNER JOIN [DBRodo].[dbo].[GLOP_PRAZOENTREGASERVICOS] PES WITH(NOLOCK)
+ ON PE.HANDLE = PES.PRAZOENTREGA
+ AND PES.SERVICO = 6 --ENTREGA PRAZO NORMAL (BUSCAR OS TIPOS DE SERVIÇO DENTRO DA GLGL_SERVICOLOGISTICA)
+
+WHERE DL.DATAEMISSAO >= '2019-10-16'
+AND DL.DATAEMISSAO < '2019-10-17'
+And ((DL.TIPODOCUMENTO In (1, 2)                                                                                                         
+And   TIPODOCUMENTOFRETE = 153)                                                                                                            
+Or  (DL.TIPODOCUMENTO In (6)                                                                                                            
+And  DL.TIPORPS <> 324))   
+AND DL.STATUS <> 236
+--AND PC.CLIENTEEPP <> 'N'
+
+
+GROUP BY 
+	DL.NUMERO,
+	DL.FILIAL,                                                                                                                                                 
+	DL.VALORTOTALRECEBER,
+	DOCCLIPESOTOTAL,
+	DOCCLIPESOCUBADOTOTAL, 
+	DOCCLIPESOCONSIDERADO,
+	DOCCLIPESOCONSIDERADO,
+	DOCCLIVOLUME,
+	DOCCLIVALORTOTAL,
+	CASE
+		WHEN 
+			(CASE
+				WHEN PC.CLIENTEEPP = 'N' THEN 'FRACIONADO'
+				--WHEN DST2.NOME = 'NATURA COSMETICOS S/A' THEN 'NATURA'
+			ELSE 'EPP'
+			END) = 'FRACIONADO' AND DST2.NOME = 'NATURA COSMETICOS S/A' THEN 'NATURA'
+		ELSE (CASE
+				WHEN PC.CLIENTEEPP = 'N' THEN 'FRACIONADO'
+				--WHEN DST2.NOME = 'NATURA COSMETICOS S/A' THEN 'NATURA'
+			ELSE 'EPP'
+			END)
+	END,
+	DST2.NOME,
+	PES.DIA
+
+) AS T1
+
+GROUP BY T1.TIPOCLIENTE,[TRANSIT TIME]
